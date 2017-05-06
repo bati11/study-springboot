7deadf3e2259ebee2817c015aa659c8958f4a992
bati11 <mail.bati11@gmail.com>
2017-05-06 15:49:20 +0900

    remove an unused endpoint

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,21 +1,10 @@
 package example;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class Application {
-    @Value("${spring.thymeleaf.cache:}")
-    private String springThymeleafCache;
-
-    @RequestMapping("/hello")
-    String home() {
-        return springThymeleafCache;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
