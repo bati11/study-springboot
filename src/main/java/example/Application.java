9b783bd7ccf6477020aa154697cfc53bd3503da1
bati11 <mail.bati11@gmail.com>
2017-05-23 22:25:47 +0900

    add MyDialect Bean

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,26 +1,16 @@
 package example;
 
 import example.view.MyDialect;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 public class Application {
 
-    @Autowired
-    ITemplateResolver templateResolver;
-
     @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setEnableSpringELCompiler(true);
-        templateEngine.setTemplateResolver(templateResolver);
-        templateEngine.addDialect(new MyDialect());
-        return templateEngine;
+    public MyDialect myDialect() {
+        return new MyDialect();
     }
 
     public static void main(String[] args) {
