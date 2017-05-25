6787cfee69888d3c72eb428c6ea695391099bba5
bati11 <mail.bati11@gmail.com>
2017-05-25 21:18:41 +0900

    modify gravatar tag arguments

--- src/main/java/example/view/GravatarProcessor.java
+++ src/main/java/example/view/GravatarProcessor.java
@@ -1,12 +1,22 @@
 package example.view;
 
 import example.MessageDigestUtil;
+import example.model.User;
+import org.thymeleaf.IEngineConfiguration;
 import org.thymeleaf.context.ITemplateContext;
 import org.thymeleaf.engine.AttributeName;
+import org.thymeleaf.model.AttributeValueQuotes;
 import org.thymeleaf.model.IModel;
 import org.thymeleaf.model.IModelFactory;
+import org.thymeleaf.model.IOpenElementTag;
 import org.thymeleaf.processor.element.*;
+import org.thymeleaf.standard.expression.IStandardExpression;
+import org.thymeleaf.standard.expression.IStandardExpressionParser;
+import org.thymeleaf.standard.expression.StandardExpressions;
 import org.thymeleaf.templatemode.TemplateMode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GravatarProcessor extends AbstractAttributeModelProcessor {
 
@@ -28,11 +38,20 @@ public class GravatarProcessor extends AbstractAttributeModelProcessor {
 
     @Override
     protected void doProcess(ITemplateContext context, IModel model, AttributeName attributeName, String attributeValue, IElementModelStructureHandler structureHandler) {
-        String gravatarId = MessageDigestUtil.md5(attributeValue);
+        IEngineConfiguration configuration = context.getConfiguration();
+        IStandardExpressionParser expressionParser = StandardExpressions.getExpressionParser(configuration);
+        IStandardExpression expression = expressionParser.parseExpression(context, attributeValue);
+        User user = (User)expression.execute(context);
+
+        String gravatarId = MessageDigestUtil.md5(user.getEmail());
         String gravatarUrl = "https://secure.gravatar.com/avatar/" + gravatarId;
 
         IModelFactory modelFactory = context.getModelFactory();
-        model.replace(0, modelFactory.createOpenElementTag("img", "src", gravatarUrl));
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("src", gravatarUrl);
+        attributes.put("alt", user.getName());
+        IOpenElementTag imgTag = modelFactory.createOpenElementTag("img", attributes, AttributeValueQuotes.DOUBLE, false);
+        model.replace(0, imgTag);
         model.remove(1);
         model.replace(model.size() - 1, modelFactory.createCloseElementTag("img"));
     }
