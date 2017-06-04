8e5252895fdff285de2acb7baa27654541135313
bati11 <mail.bati11@gmail.com>
2017-06-04 17:43:52 +0900

    modify user form validations

--- src/main/java/example/controllers/forms/UserForm.java
+++ src/main/java/example/controllers/forms/UserForm.java
@@ -4,8 +4,9 @@ import example.controllers.forms.validators.EqualsPropertyValues;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Data
@@ -15,16 +16,18 @@ public class UserForm implements Serializable {
 
     public static final long serialVersionUID = 1L;
 
-    @Length(min=1, max=50)
+    @NotBlank
+    @Size(max=50)
     private String name;
 
-    @Length(min=6, max=255)
+    @NotBlank
+    @Size(max=255)
     @Email
     private String email;
 
-    @Length(min=6, max=255)
+    @Size(min=6, max=255)
     private String password;
 
-    @Length(min=6, max=255)
+    @Size(min=6, max=255)
     private String passwordConfirmation;
 }
