2e538b0db1cc1d414a2a9fd08aa018bece52f0f2
bati11 <mail.bati11@gmail.com>
2017-06-24 17:45:09 +0900

    Change PasswordEncoder

--- src/main/java/example/auth/PasswordDigestFactory.java
+++ src/main/java/example/auth/PasswordDigestFactory.java
@@ -1,13 +1,11 @@
 package example.auth;
 
-import example.MessageDigestUtil;
 import lombok.NonNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordDigestFactory {
-
-    private final String salt= "SALT";
 
     public PasswordDigest fromDigest(@NonNull String digest) {
         PasswordDigest result = new PasswordDigest(digest);
@@ -15,7 +13,8 @@ public class PasswordDigestFactory {
     }
 
     public PasswordDigest create(@NonNull String rawPassword) {
-        String value = MessageDigestUtil.sha256(rawPassword + salt);
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        String value = bCryptPasswordEncoder.encode(rawPassword);
         PasswordDigest result = new PasswordDigest(value);
         return result;
     }
--- src/main/java/example/springconfig/SecurityConfig.java
+++ src/main/java/example/springconfig/SecurityConfig.java
@@ -1,8 +1,8 @@
 package example.springconfig;
 
-import example.auth.MyPasswordEncoder;
 import example.auth.LoginAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -52,13 +54,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
-    MyPasswordEncoder myPasswordEncoder;
-
-    @Autowired
     LoginAccountRepository loginAccountRepository;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Autowired
     public void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(loginAccountRepository).passwordEncoder(myPasswordEncoder);
+        auth.userDetailsService(loginAccountRepository).passwordEncoder(passwordEncoder());
     }
 }
