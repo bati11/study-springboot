65ad076eebca3447329de56ba7ccc59877455282
bati11 <mail.bati11@gmail.com>
2017-06-25 15:46:18 +0900

    add forwardedHeaderFilter

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -2,9 +2,21 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class Application {
+
+    /**
+     * for reverse proxy
+     */
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        return filter;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
